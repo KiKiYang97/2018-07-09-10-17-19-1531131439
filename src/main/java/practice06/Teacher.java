d8240ed810b49891d2872988ebce237547da427a
YANGKI4 <dunka97@163.com>
2020-07-22 15:59:12 +0800

    [practice06][KikiYang][feature]

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,31 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Integer klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Integer kclass) {
+        super(name, age);
+        this.klass = kclass;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Integer kclass) {
+        this.klass = kclass;
+    }
+
+    public String introduce(){
+        if(klass!=null){
+            return super.introduce()+" I am a Teacher. I teach Class "+klass+".";
+        }
+        else{
+            return super.introduce()+" I am a Teacher. I teach No Class.";
+        }
+    }
 }
