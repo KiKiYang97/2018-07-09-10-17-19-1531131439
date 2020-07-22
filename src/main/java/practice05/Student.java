b72117695e8e3bbcc88ba504d3b9bca15b565c4c
YANGKI4 <dunka97@163.com>
2020-07-22 15:51:33 +0800

    [practice05][KikiYang][feature     ]

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,27 @@
 package practice05;
 
-public class Student {
+public class Student extends Person{
+    private int klass;
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, int kclass) {
+        super(name, age);
+        this.klass = kclass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int kclass) {
+        this.klass = kclass;
+    }
+
+    public String introduce(){
+        return super.introduce()+" I am a Student. I am at Class "+klass+".";
+    }
 }
+
