761479fb837c7f3ba41e059f05e72819b9f7e060
Dunka97 <1150311882@qq.com>
2020-07-22 21:26:43 +0800

    [pratice10][KikiYang][feature]

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,49 @@
 package practice10;
 
+
+
 public class Klass {
+    private Integer number;
+    private Student leader;
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public Student assignLeader(Student student){
+        if(!this.number.equals(student.getKlass().getNumber())){
+            System.out.print("It is not one of us.\n");
+            return null;
+        }
+        this.leader = student;
+        return student;
+    }
+
+    public String getDisplayName() {
+        return "Class "+number;
+    }
+
+    public void appendMember(Student student){
+
+    }
+
+    public boolean isIn(Student student) {
+        if(student.getKlass().getNumber().equals(this.number)){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,64 @@
 package practice10;
 
-public class Teacher {
+
+import java.util.Collection;
+
+public class Teacher extends Person{
+    private Collection<Klass> classes;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, Collection<Klass> klassCollection) {
+        super(id, name, age);
+        this.classes = klassCollection;
+    }
+
+    public Collection<Klass> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(Collection<Klass> classes) {
+        this.classes = classes;
+    }
+
+    public String introduce(){
+        if(classes.size()>0){
+            return super.introduce()+" I am a Teacher. I teach Class "+getEveryKlass(classes)+".";
+        }
+        else{
+            return super.introduce()+" I am a Teacher. I teach No Class.";
+        }
+    }
+
+    private String getEveryKlass(Collection<Klass> klassCollection) {
+        String everyKlass = "";
+        for(Klass klass:klassCollection){
+            everyKlass += klass.getNumber() + ",";
+        }
+        return everyKlass.substring(0,everyKlass.length()-1);
+    }
+
+    public boolean isTeaching(Student student){
+        final boolean[] flag = {false};
+        classes.forEach(klass -> {
+            if(klass.isIn(student)){
+                flag[0] = true;
+            }
+        });
+        return flag[0];
+    }
+
+    public String introduceWith(Student student){
+        String introduce = "";
+        for(Klass clazz:classes){
+            if(student.getKlass().equals(clazz)){
+                introduce = super.introduce()+" I am a Teacher. I teach "+student.getName()+".";
+            }else{
+                introduce = super.introduce()+" I am a Teacher. I don't teach "+student.getName()+".";
+            }
+        }
+        return introduce;
+    }
 }
